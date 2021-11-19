8dbd6e4d4522e406b71ae44b53b84eb06dd19091
Rob Kooper <kooper@illinois.edu>
2021-11-19 12:06:57 -0600

    remove commented out options

--- terraform/modules/rke2/rancher.tf
+++ terraform/modules/rke2/rancher.tf
@@ -7,82 +7,13 @@ resource "rancher2_cluster_v2" "kube" {
   default_cluster_role_for_project_members = "user"
 
   rke_config {
-#     chart_values = <<EOF
-# rke2-calico:
-#   calicoctl:
-#     image: rancher/mirrored-calico-ctl
-#     tag: v3.19.2
-#   certs:
-#     node:
-#       cert: null
-#       commonName: null
-#       key: null
-#     typha:
-#       caBundle: null
-#       cert: null
-#       commonName: null
-#       key: null
-#   felixConfiguration:
-#     featureDetectOverride: ChecksumOffloadBroken=true
-#   global:
-#     clusterCIDRv4: ""
-#     clusterCIDRv6: ""
-#     systemDefaultRegistry: ""
-#   imagePullSecrets: {}
-#   installation:
-#     calicoNetwork:
-#       bgp: Disabled
-#       ipPools:
-#       - blockSize: 24
-#         cidr: 10.42.0.0/16
-#         encapsulation: VXLAN
-#         natOutgoing: Enabled
-#     controlPlaneTolerations:
-#     - effect: NoSchedule
-#       key: node-role.kubernetes.io/control-plane
-#       operator: Exists
-#     - effect: NoExecute
-#       key: node-role.kubernetes.io/etcd
-#       operator: Exists
-#     enabled: true
-#     imagePath: rancher
-#     imagePrefix: mirrored-calico-
-#     kubernetesProvider: ""
-#   ipamConfig:
-#     autoAllocateBlocks: true
-#     strictAffinity: true
-#   tigeraOperator:
-#     image: rancher/mirrored-calico-operator
-#     registry: docker.io
-#     version: v1.17.6
-# EOF
-    # etcd {
-    #   snapshot_schedule_cron = "0 */5 * * *"
-    #   snapshot_retention = 5
-    # }
     local_auth_endpoint {
-      # ca_certs = ""
       enabled  = var.cluster_direct_access
-      # fqdn     = ""
     }
-#     machine_global_config = <<EOF
-# cni: "calico"
-# disable-kube-proxy: false
-# etcd-expose-metrics: false
-# disable:
-# - rke2-ingress-nginx
-# EOF
     machine_global_config = <<EOF
 disable:
 - rke2-ingress-nginx
 EOF
-    # machinePools: []
-    # machineSelectorConfig:
-    # - config:
-    #     protect-kernel-defaults: false
-    # registries:
-    #   configs: {}
-    #   mirrors: {}
     upgrade_strategy {
       control_plane_concurrency = 1
       control_plane_drain_options {
