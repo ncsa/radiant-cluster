f7bc42aa66810246aa75f915d95a26d4fac66b85
Rob Kooper <kooper@illinois.edu>
2021-12-09 17:48:49 -0600

    create rke1 cluster

---
+++ terraform/modules/rke1/rancher.tf
@@ -0,0 +1,154 @@
+# ----------------------------------------------------------------------
+# cluster definition
+# ----------------------------------------------------------------------
+resource "rancher2_cluster" "kube" {
+  name        = var.cluster_name
+  description = var.cluster_description
+  driver      = "rancherKubernetesEngine"
+
+  cluster_auth_endpoint {
+    enabled = false
+  }
+
+  rke_config {
+    kubernetes_version = var.rke1_version
+    network {
+      plugin = "weave"
+    }
+    ingress {
+      provider = "none"
+    }
+    upgrade_strategy {
+      drain = true
+      drain_input {
+        delete_local_data  = true
+        ignore_daemon_sets = true
+        timeout            = 120
+      }
+      max_unavailable_controlplane = 1
+      max_unavailable_worker       = 1
+    }
+  }
+}
+
+# Create a new rancher2 Cluster Sync for foo-custom cluster
+resource "rancher2_cluster_sync" "kube" {
+  depends_on    = [openstack_compute_instance_v2.controlplane[0]]
+  cluster_id    = rancher2_cluster.kube.id
+  wait_catalogs = false
+}
+
+# ----------------------------------------------------------------------
+# cluster access
+# ----------------------------------------------------------------------
+resource "rancher2_cluster_role_template_binding" "admin_users" {
+  for_each          = var.admin_users
+  name              = "${rancher2_cluster_sync.kube.id}-user-${replace(each.value, "_", "-")}"
+  cluster_id        = rancher2_cluster_sync.kube.id
+  role_template_id  = "cluster-owner"
+  user_principal_id = "openldap_user://uid=${each.value},ou=People,dc=ncsa,dc=illinois,dc=edu"
+  lifecycle {
+    ignore_changes = [
+      annotations,
+      labels,
+      user_id
+    ]
+  }
+}
+
+resource "rancher2_cluster_role_template_binding" "admin_groups" {
+  for_each          = var.admin_groups
+  name              = "${rancher2_cluster_sync.kube.id}-group-${replace(each.value, "_", "-")}"
+  cluster_id        = rancher2_cluster_sync.kube.id
+  role_template_id  = "cluster-owner"
+  user_principal_id = "openldap_group://cn=${each.value},ou=Groups,dc=ncsa,dc=illinois,dc=edu"
+  lifecycle {
+    ignore_changes = [
+      annotations,
+      labels,
+      user_id
+    ]
+  }
+}
+
+resource "rancher2_cluster_role_template_binding" "member_users" {
+  for_each          = var.member_users
+  name              = "${rancher2_cluster_sync.kube.id}-user-${replace(each.value, "_", "-")}"
+  cluster_id        = rancher2_cluster_sync.kube.id
+  role_template_id  = "cluster-member"
+  user_principal_id = "openldap_user://uid=${each.value},ou=People,dc=ncsa,dc=illinois,dc=edu"
+  lifecycle {
+    ignore_changes = [
+      annotations,
+      labels,
+      user_id
+    ]
+  }
+}
+
+resource "rancher2_cluster_role_template_binding" "member_groups" {
+  for_each          = var.member_groups
+  name              = "${rancher2_cluster_sync.kube.id}-group-${replace(each.value, "_", "-")}"
+  cluster_id        = rancher2_cluster_sync.kube.id
+  role_template_id  = "cluster-member"
+  user_principal_id = "openldap_group://cn=${each.value},ou=Groups,dc=ncsa,dc=illinois,dc=edu"
+  lifecycle {
+    ignore_changes = [
+      annotations,
+      labels,
+      user_id
+    ]
+  }
+}
+
+# ----------------------------------------------------------------------
+# longhorn storage
+# ----------------------------------------------------------------------
+resource "rancher2_app_v2" "longhorn-system" {
+  count      = var.longhorn_enabled ? 1 : 0
+  cluster_id = rancher2_cluster_sync.kube.cluster_id
+  name       = "longhorn"
+  namespace  = "longhorn-system"
+  repo_name  = "rancher-charts"
+  chart_name = "longhorn"
+  project_id = rancher2_cluster_sync.kube.system_project_id
+  values     = <<EOF
+defaultSettings:
+  backupTarget: nfs://radiant-nfs.ncsa.illinois.edu:/radiant/projects/${data.openstack_identity_auth_scope_v3.scope.project_name}/${var.cluster_name}/backup
+  defaultReplicaCount: ${var.longhorn_replicas}
+persistence:
+  defaultClass: false
+  defaultClassReplicaCount: ${var.longhorn_replicas}
+EOF
+  lifecycle {
+    ignore_changes = [
+      values
+    ]
+  }
+}
+
+# ----------------------------------------------------------------------
+# monitoring
+# ----------------------------------------------------------------------
+resource "rancher2_app_v2" "monitor" {
+  count      = var.monitoring_enabled ? 1 : 0
+  cluster_id = rancher2_cluster_sync.kube.cluster_id
+  name       = "rancher-monitoring"
+  namespace  = "cattle-monitoring-system"
+  repo_name  = "rancher-charts"
+  chart_name = "rancher-monitoring"
+  project_id = rancher2_cluster_sync.kube.system_project_id
+  //  values        = <<EOF
+  //prometheus:
+  //  resources:
+  //    core:
+  //      limits:
+  //        cpu: "4000m"
+  //        memory: "6144Mi"
+  //EOF
+  lifecycle {
+    ignore_changes = [
+      values
+    ]
+  }
+}
---
+++ terraform/modules/rke1/security_group.tf
@@ -0,0 +1,128 @@
+resource "openstack_networking_secgroup_v2" "cluster_security_group" {
+  name        = var.cluster_name
+  description = "${var.cluster_name} kubernetes cluster security group"
+}
+
+# ----------------------------------------------------------------------
+# Egress
+# ----------------------------------------------------------------------
+
+#Egress  IPv4  Any Any 0.0.0.0/0 - - 
+#resource "openstack_networking_secgroup_rule_v2" "egress_ipv4" {
+#  direction         = "egress"
+#  ethertype         = "IPv4"
+#  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+#  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+#}
+
+#Egress  IPv6  Any Any ::/0  - - 
+#resource "openstack_networking_secgroup_rule_v2" "egress_ipv6" {
+#  direction         = "egress"
+#  ethertype         = "IPv6"
+#  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+#  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+#}
+
+# ----------------------------------------------------------------------
+# Ingress
+# ----------------------------------------------------------------------
+
+# Ingress IPv4  ICMP  Any 0.0.0.0/0 - - 
+resource "openstack_networking_secgroup_rule_v2" "ingress_icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+# Ingress IPv4  TCP 22 (SSH)  0.0.0.0/0 - - 
+resource "openstack_networking_secgroup_rule_v2" "ingress_ssh" {
+  description       = "ssh"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+# Ingress IPv4  TCP 80 (HTTP) 0.0.0.0/0 - - 
+resource "openstack_networking_secgroup_rule_v2" "ingress_http" {
+  description       = "http"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 80
+  port_range_max    = 80
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+# Ingress IPv4  TCP 443 (HTTPS) 0.0.0.0/0 - - 
+resource "openstack_networking_secgroup_rule_v2" "ingress_https" {
+  description       = "https"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+# Ingress IPv4  TCP 6443  141.142.0.0/16  - kube api  
+resource "openstack_networking_secgroup_rule_v2" "ingress_kubeapi" {
+  description       = "kubeapi"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "141.142.0.0/16"
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+# Ingress IPv4  TCP 30000 - 32767 0.0.0.0/0 - nodeport  
+resource "openstack_networking_secgroup_rule_v2" "ingress_nodeport" {
+  description       = "nodeport"
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 30000
+  port_range_max    = 32767
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+resource "openstack_networking_secgroup_rule_v2" "same_security_group_ingress_tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  remote_group_id   = openstack_networking_secgroup_v2.cluster_security_group.id
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+resource "openstack_networking_secgroup_rule_v2" "same_security_group_ingress_udp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  remote_group_id   = openstack_networking_secgroup_v2.cluster_security_group.id
+  security_group_id = openstack_networking_secgroup_v2.cluster_security_group.id
+  depends_on        = [openstack_networking_secgroup_v2.cluster_security_group]
+}
+
+
+# Ingress IPv4  TCP 1 - 65535 192.168.100.0/24  - - 
+# Ingress IPv4  TCP 1 - 65535 141.142.216.0/21  - - 
+# Ingress IPv4  TCP 2376  141.142.0.0/16  - - 
+# Ingress IPv4  TCP 2379 - 2380 141.142.0.0/16  - etcd  
+# Ingress IPv4  TCP 5201  0.0.0.0/0 - - 
+# Ingress IPv4  TCP 6783  141.142.0.0/16  - weave 
+# Ingress IPv4  TCP 10250 - 10255 141.142.0.0/16  - kubelet 
+# Ingress IPv4  UDP 1 - 65535 192.168.100.0/24  - - 
+# Ingress IPv4  UDP 1 - 65535 141.142.216.0/21  - - 
+# Ingress IPv4  UDP 6783 - 6784 141.142.0.0/16  - weave 
